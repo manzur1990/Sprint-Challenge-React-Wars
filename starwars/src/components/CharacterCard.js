22dfae0a05dba0d4aa1b70d839e64361b06709d2
JM <manzur-1990@hotmail.com>
2020-04-17 13:26:25 -0500

    Questions answered

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -3,15 +3,16 @@ import { card } from 'reactstrap';
 import { Card, CardBody, CardTitle, CardText } from "reactstrap";
 
 
+
 const CharacterCard = props => {
 
     return (
 
         <div className="personCard">
             <Card>
-                <CardBody>
 
-                    <CardTitle>My name is {props.name}</CardTitle>
+                <CardTitle>My name is {props.name}</CardTitle>
+                <CardBody>
                     <CardText>I am '{props.height}' cm tall</CardText>
                     <CardText>I have {props.eyeColor} eye color</CardText>
                     <CardText>and I was born in {props.birthdate}, </CardText>
